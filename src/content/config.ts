7610f7030f683d7f8cd2581cac7763164c5501f6
xyny <git@xyny.anonaddy.me>
2024-12-26 20:20:31 +0200

    feat: most of windows xp and some upgrades
    still some stuff todo

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,35 +1,44 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
 export const collections = {
-  'portfolio': defineCollection({
-        type: 'content',
-        schema: ({ image }) =>  z.object({
-            title: z.string(),
-            desc: z.string(),
-            gallery: z.array(z.object({
-                sectiontitle: z.string(),
-                sectiondesc: z.string(),
-                images: z.array(z.object({
-                    img: image(),
-                    alt: z.string(),
-                    caption: z.string()
-                }))
-            }))
-        })
-  }),
-  'about': defineCollection({
-    type: 'content',
-    schema: () =>  z.object({
-        shortbio: z.string(),
-        subtexts: z.array(z.string())
-    })
-   }),
-   'links': defineCollection({
-    type: 'data',
-    schema: () =>  z.object({
-        links: z.array(z.object({
-            name: z.string(),
-            url: z.string()
-        }))
-    })
-   }),
+    portfolio: defineCollection({
+        type: "content",
+        schema: ({ image }) =>
+            z.object({
+                title: z.string(),
+                desc: z.string(),
+                gallery: z.array(
+                    z.object({
+                        sectiontitle: z.string(),
+                        sectiondesc: z.string(),
+                        images: z.array(
+                            z.object({
+                                img: image(),
+                                alt: z.string(),
+                                caption: z.string(),
+                            })
+                        ),
+                    })
+                ),
+            }),
+    }),
+    about: defineCollection({
+        type: "content",
+        schema: () =>
+            z.object({
+                shortbio: z.string(),
+                subtexts: z.array(z.string()),
+            }),
+    }),
+    links: defineCollection({
+        type: "data",
+        schema: () =>
+            z.object({
+                links: z.array(
+                    z.object({
+                        name: z.string(),
+                        url: z.string(),
+                    })
+                ),
+            }),
+    }),
 };
