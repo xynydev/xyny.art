0292f406e68dacf70bb49ff8e3102c30f597775e
xyny <git@xyny.art>
2026-08-12 17:01:57 +0300

    fix: better pixelfed

--- src/util/fetchPixelfedFeed.ts
+++ src/util/fetchPixelfedFeed.ts
@@ -14,35 +14,41 @@ export async function fetchPixelfedFeed() {
                     .querySelector("feed")
                     .querySelectorAll("entry")
             ).map(async (entry) => {
-                const url = entry.querySelector("id").textContent;
+              const url = entry.querySelector("id").textContent;
 
-                const date = new Date(
-                    entry.querySelector("updated").textContent
-                );
+              const date = new Date(
+                  entry.querySelector("updated").textContent
+              );
 
-                const id = url.split("/").at(-1);
-                const res = await fetch(
-                    "https://metapixl.com/api/v2/profile/xyny/status/" + id
-                );
-                const json = await res.json();
+              const id = url.split("/").at(-1);
+              const res = await fetch(
+                  "https://metapixl.com/api/v2/profile/xyny/status/" + id
+              );
+              const json = await res.json();
 
-                const images = json.status.media_attachments.map((i) => {
-                    return { url: i.url, alt: i.description };
-                });
+              const images = json.status.media_attachments.map((i) => {
+                  return { url: i.url, alt: i.description };
+              });
 
-                const text = new JSDOM(
-                    entry.querySelector("content").textContent,
-                    {
-                        contentType: "text/html",
-                        storageQuota: 10000000,
-                    }
-                ).window.document
-                    .querySelector("p")
-                    .innerHTML.replace(/<a[^>]*>/g, "")
-                    .replaceAll("</a>", "")
-                    .replace("<br>\n<br>", "<br>");
+              const domText = new JSDOM(
+                  entry.querySelector("content").textContent,
+                  {
+                      contentType: "text/html",
+                      storageQuota: 10000000,
+                  }
+              ).window.document
+                  .querySelector("p")
+                  .innerHTML
+                  .replace(/<a[^>]*>/g, "")
+                  .replaceAll("</a>", "")
+                  .replace("<br>\n<br>", "<br>");
 
-                return { text, url, date, images };
+              const text = domText.split("\n")
+                .filter(p => !p.includes("#"))
+                .filter(p => p.length > 1)
+                .join("\n")
+
+              return { text, url, date, images };
             })
         );
         return feed;
@@ -55,8 +61,8 @@ export async function fetchPixelfedFeed() {
                 date: new Date(),
                 images: [
                     {
-                        url: "https://xyny.art/_astro/motionatfrontdoor.CKhmc63n_24Dgk5.webp",
-                        alt: "a very blue image with a digital pattern of lines in a grid sometimes going vertically, sometimes horizontally. some rainbow streaks go through the image. at the bottom there seems to be a sphynx cat (allegedly floppa bingus) silently waiting for the owner of the doorbell camera to come open up the door",
+                        url: "https://xyny.art/og_image.png",
+                        alt: "XYNY",
                     },
                 ],
             },
